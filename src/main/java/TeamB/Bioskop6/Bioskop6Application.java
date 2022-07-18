3e8fd2706850cfa13717f1534f8c8a0454f7db24
billymarcelino058 <billymarcelino058@gmail.com>
2022-07-18 10:04:53 +0700

    inisialisasi spring bioskop6

---
+++ src/main/java/TeamB/Bioskop6/Bioskop6Application.java
@@ -0,0 +1,13 @@
+package TeamB.Bioskop6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bioskop6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bioskop6Application.class, args);
+	}
+
+}
---
+++ src/test/java/TeamB/Bioskop6/Bioskop6ApplicationTests.java
@@ -0,0 +1,13 @@
+package TeamB.Bioskop6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bioskop6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
